02908230ebd75231eabbb6c8e0e1864b7e72fc23
Svetlana <89111378147@yandex.ru>
2021-12-28 14:49:46 +0300

    Successful registration to demowebshop (API + UI) added

--- src/test/java/DemoWebShopTests.java
+++ src/test/java/DemoWebShopTests.java
@@ -1,16 +1,23 @@
 import com.codeborne.selenide.Condition;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.get;
+
 
 public class DemoWebShopTests {
+
 
     // test for a new user, always 1 item in the cart
     @Test
@@ -89,44 +96,38 @@ public class DemoWebShopTests {
         step("Check that item has been added to Shopping Cart by UI", () -> {
             open("http://demowebshop.tricentis.com/cart");
             $(".cart-item-row").shouldBe(Condition.visible);
-            $(".product-name").shouldHave(Condition.text("Build your own expensive computer"));
+            $(".product-name").shouldHave(text("Build your own expensive computer"));
         });
     }
 
 
-    //  @Test
-    //  @Tag("demowebshop")
-    //  @Disabled("Example test code for further test development")
-    //  @DisplayName("Successful authorization to some demowebshop (API + UI)")
-    // void loginWithCookieTest() {
-    //     step("Get cookie by api and set it to browser", () -> {
-    //       String authorizationCookie =
-    //              given()
-    //                     .filter(AllureRestAssuredFilter.withCustomTemplates())
-    //                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-    //                  .formParam("Email", App.config.userLogin())
-    //                .formParam("Password", App.config.userPassword())
-    //              .when()
-    //                      .post("/login")
-    //                      .then()
-    //                      .statusCode(302)
-    //                      .extract()
-    //                      .cookie("NOPCOMMERCE.AUTH");
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful registration to demowebshop (API + UI)")
+    void registration() {
+        step("Register user by API", () -> {
+            given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .body("__RequestVerificationToken=EOKFnxmSoeEnFm-" +
+                            "ghPa0CZir4ETJXEXwMfTdzZIgoS55Tm0oKwyiDESnXdIbi-6DgkSCWGC_" +
+                            "HPyc5ka4gNvFApBjYYmWJrNVgUYFgl66VzA1&Gender=" +
+                            "F&FirstName=Sveta&LastName=Andreeva&Email=sve7987%40yandex." +
+                            "ru&Password=123456&ConfirmPassword=123456&register-button=Register")
+                    .when()
+                    .post("http://demowebshop.tricentis.com/register")
+                    .then()
+                    .statusCode(302);
+            get("http://demowebshop.tricentis.com/registerresult/1")
+                    .then()
+                    .statusCode(200);
+        });
 
-    //     step("Open minimal content, because cookie can be set when site is opened", () ->
-    //              open("/Themes/DefaultClean/Content/images/logo.png"));
+        step("Check that registration successed by UI", () -> {
+            open("http://demowebshop.tricentis.com/registerresult/1");
+            $(".result").shouldHave(Condition.text("Your registration completed"));
+        });
+    }
 
-    //      step("Set cookie to to browser", () ->
-    //              getWebDriver().manage().addCookie(
-    //                      new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
-    //  });
-
-    //  step("Open main page", () ->
-    //          open(""));
-
-    //  step("Verify successful authorization", () ->
-    //          $(".account").shouldHave(text(App.config.userLogin())));
-    //}
 
 }
 
