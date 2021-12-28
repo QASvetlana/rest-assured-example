f3213e10e57c216adabf91fe463bf6c0847fb200
Svetlana <89111378147@yandex.ru>
2021-12-28 13:33:35 +0300

    api and ui tests fixed

--- src/test/java/DemoWebShopTests.java
+++ src/test/java/DemoWebShopTests.java
@@ -1,12 +1,12 @@
-
+import com.codeborne.selenide.Condition;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
@@ -28,7 +28,6 @@ public class DemoWebShopTests {
                         .body("success", is(true))
                         .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
                         .body("updatetopcartsectionhtml", is("(1)"))
-                        // достать ответ
                         .extract().response();
 
         System.out.println("Response: " + response.path("updatetopcartsectionhtml"));
@@ -48,7 +47,7 @@ public class DemoWebShopTests {
                                 "product_attribute_16_8_8=22&addtocart_16.EnteredQuantity=1")
                         .cookie("Nop.customer=4501cf08-78ac-4d9b-8381-79dfdb3f85a9;")
                         .when()
-                        .post("http://demowebshop.tricentis.com/addproducttocart/details/16/1")
+                        .post("/addproducttocart/details/74/1")
                         .then()
                         .statusCode(200)
                         .body("success", is(true))
@@ -68,7 +67,15 @@ public class DemoWebShopTests {
     void addProductToCartWithCookieAPIandUITest() {
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("Nop.customer=957e0607-7b6f-4e93-8308-32ac30a6677c;")
+                .body("product_attribute_74_5_26=82" +
+                        "&product_attribute_74_6_27=85" +
+                        "&product_attribute_74_3_28=87" +
+                        "&product_attribute_74_8_29=88" +
+                        "&product_attribute_74_8_29=89" +
+                        "&product_attribute_74_8_29=90" +
+                        "&addtocart_74.EnteredQuantity=2")
+                .cookie("Nop.customer=69589107-6373-41bd-891d-47fb44277adc;")
+                .when()
                 .post("http://demowebshop.tricentis.com/addproducttocart/details/31/1")
                 .then()
                 .statusCode(200)
@@ -78,11 +85,48 @@ public class DemoWebShopTests {
 // Add cookie to browser
         open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
         getWebDriver().manage().addCookie(
-                new Cookie("Nop.customer", "957e0607-7b6f-4e93-8308-32ac30a6677c"));
-        open("http://demowebshop.tricentis.com/cart");
-        $(".product-name").shouldHave(text("Build your own cheap computer"));
+                new Cookie("Nop.customer", "69589107-6373-41bd-891d-47fb44277adc"));
+        step("Check that item has been added to Shopping Cart by UI", () -> {
+            open("http://demowebshop.tricentis.com/cart");
+            $(".cart-item-row").shouldBe(Condition.visible);
+            $(".product-name").shouldHave(Condition.text("Build your own expensive computer"));
+        });
     }
 
+
+    //  @Test
+    //  @Tag("demowebshop")
+    //  @Disabled("Example test code for further test development")
+    //  @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    // void loginWithCookieTest() {
+    //     step("Get cookie by api and set it to browser", () -> {
+    //       String authorizationCookie =
+    //              given()
+    //                     .filter(AllureRestAssuredFilter.withCustomTemplates())
+    //                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+    //                  .formParam("Email", App.config.userLogin())
+    //                .formParam("Password", App.config.userPassword())
+    //              .when()
+    //                      .post("/login")
+    //                      .then()
+    //                      .statusCode(302)
+    //                      .extract()
+    //                      .cookie("NOPCOMMERCE.AUTH");
+
+    //     step("Open minimal content, because cookie can be set when site is opened", () ->
+    //              open("/Themes/DefaultClean/Content/images/logo.png"));
+
+    //      step("Set cookie to to browser", () ->
+    //              getWebDriver().manage().addCookie(
+    //                      new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+    //  });
+
+    //  step("Open main page", () ->
+    //          open(""));
+
+    //  step("Verify successful authorization", () ->
+    //          $(".account").shouldHave(text(App.config.userLogin())));
+    //}
 
 }
 
